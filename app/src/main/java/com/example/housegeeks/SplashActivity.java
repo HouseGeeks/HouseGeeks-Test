ceb7582a2516ab0278cae00249c751eaab1716fd
Emmanuel Onung <onungemmanuel@gmail.com>
2022-09-09 19:11:25 -0500

    Splash Screen Created

---
+++ app/src/main/java/com/example/housegeeks/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.housegeeks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.imageViewlogo);
+        Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide);
+        logo.startAnimation(slideAnimation);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },900);
+    }
+}
